2a5325be8fc41c667f8b022f64a4840cb5055981
nibedita21-ai <nibeditaswain351@gmail.com>
2024-08-15 22:30:47 +0530

    add new paragraph

--- code13.py
+++ code13.py
@@ -1,6 +1,6 @@
-weight_as_int = int(85)
+weight_as_int = int(75)
 height_as_float = float(1.8)
 
 bmi = (weight_as_int//height_as_float ** 2 )
 
-print(bmi)
+print(int(bmi))                                                                                                                                                     
