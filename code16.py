0838138a1146c1130dee41ef88fc3ac95e565010
nibedita21-ai <nibeditaswain351@gmail.com>
2024-08-22 21:03:43 +0530

    add paragraph

---
+++ code16.py
@@ -0,0 +1,13 @@
+# problem 1
+points_for_win = 7
+points_for_draw = 2
+points_for_loss = 0
+
+wins = 4
+draws = 1
+losses = 3
+
+total_points = (wins * points_for_win) + (draws * points_for_draw) + (losses * points_for_loss)
+
+print("The total number of points gained by Team Aravali is:", total_points)
+
