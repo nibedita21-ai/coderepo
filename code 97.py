2e063b2a306d073da820ec7725f5b4f863be44cb
nibedita21-ai <nibeditaswain351@gmail.com>
2025-04-21 12:22:51 +0530

    code 97

---
+++ code 97.py
@@ -0,0 +1,8 @@
+a = 12
+b = 2
+
+print("the value of", a, "+", b, "is-" , a+b)
+print("the value of", a, "-", b, "is-" , a-b)
+print("the value of", a, "*", b, "is-" , a*b)
+print("the value of", a, "/", b, "is-" , a/b)
+print("the value of", a, "%", b, "is-" , a%b)
